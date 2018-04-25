602328bd078aa2bc8bdef1b734a514b057f22749
Megh <meghshah09@gmail.com>
2018-04-25 13:49:16 -0400

    Initial Commit

---
+++ src/genericCheckpointing/xmlStoreRestore/StoreRestoreHandler.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package genericCheckpointing.xmlStoreRestore;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+
+/**
+ *
+ * @author Megh Shah
+ */
+public class StoreRestoreHandler implements InvocationHandler{
+
+    @Override
+    public Object invoke(Object o, Method method, Object[] os) throws Throwable {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
